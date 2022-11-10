b049b0f23f43dd8a3caadeb5669be346faed0104
Leopold Johannes Meinel <leo@meinel.dev>
2022-11-10 05:07:42 +0100

    Add ECS
    Sources:
    - https://bevyengine.org/learn/book/getting-started/ecs/

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,86 @@
  * -----
  */
 use bevy::prelude::*;
+use colored::Colorize;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(add_npcs)
+        .add_startup_system(add_players.after(add_npcs))
+        .add_system(list_npcs)
+        .add_system(list_players.after(list_npcs))
+        .run();
+}
+
+// Characters
+#[derive(Component)]
+struct Npc;
+
+#[derive(Component)]
+struct Player;
+
+// Player - data
+#[derive(Component)]
+struct PlayerIpAddr(IpAddr);
+
+// Characters - data
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Health(u8);
+
+#[derive(Component)]
+struct Killable(bool);
+
+fn add_npcs(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Npc)
+        .insert(Name("Fisherman".to_string()))
+        .insert(Health(100))
+        .insert(Killable(true));
+}
+
+fn list_npcs(query: Query<(&Name, &Health, &Killable), With<Npc>>) {
+    println!("{}", "NPCs".bold());
+    let iter = query.iter();
+    for (i, (name, health, killable)) in &mut iter.enumerate() {
+        println!("\tID {}:", &i);
+        println!("\t\tName:\t\t{}", name.0);
+        println!("\t\tHealth:\t\t{}", health.0);
+        println!("\t\tKillable:\t{}", killable.0);
+    }
+}
+
+fn add_players(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Player)
+        .insert(Name("player-0".to_string()))
+        .insert(Health(100))
+        .insert(Killable(true))
+        .insert(PlayerIpAddr(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1))));
+    commands
+        .spawn()
+        .insert(Player)
+        .insert(Name("player-1".to_string()))
+        .insert(Health(100))
+        .insert(Killable(true))
+        .insert(PlayerIpAddr(IpAddr::V6(Ipv6Addr::new(
+            0, 0, 0, 0, 0, 0, 0, 1,
+        ))));
+}
+
+fn list_players(query: Query<(&Name, &Health, &Killable, &PlayerIpAddr), With<Player>>) {
+    println!("{}", "PLAYERS".bold());
+    let iter = query.iter();
+    for (i, (name, health, killable, player_ip_addr)) in &mut iter.enumerate() {
+        println!("\tID {}:", &i);
+        println!("\t\tName:\t\t{}", name.0);
+        println!("\t\tHealth:\t\t{}", health.0);
+        println!("\t\tKillable:\t{}", killable.0);
+        println!("\t\tIP:\t\t{}", player_ip_addr.0);
+    }
 }
